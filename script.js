e351c9a8fb452634040772bc133660bcde50890f
pranav200921 <143499872+pranav200921@users.noreply.github.com>
2023-09-14 14:05:02 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ function showDetails(itemId) {
     // You can use AJAX or fetch data from a JSON file or database
     // For this example, let's assume you have an array of item details
     const itemData = [
-        { id: 1, title: 'Item 1', description: 'Details of Item 1' },
-        { id: 2, title: 'Item 2', description: 'Details of Item 2' },
+        { id: 1, title: 'Auto 1', description: 'Details of Auto 1' },
+        { id: 2, title: 'Auto 2', description: 'Details of Auto 2' },
+        { id: 2, title: 'Auto 3', description: 'Details of Auto 3' },
         // Add more item details as needed
     ];
 
